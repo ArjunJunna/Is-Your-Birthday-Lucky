028b0bac17028cc259cc01e0b087f570581c6cce
ArjunJunna <arjun9852@gmail.com>
2021-08-18 10:30:39 +0530

    feat: added message feature

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ const luckyNumber = document.querySelector('#luckyNumber');
 const btn = document.querySelector('#btn');
 const output = document.querySelector('#output');
 const reset = document.querySelector('#reset');
+const message = document.querySelector('.message');
 const policy = document.querySelector('#policy');
 const policyCard = document.querySelector('#policy-card');
-
+hideMessage();
 btn.addEventListener('click', function () {
   let date = birthDate.value;
   if (date === '') {
-    alert('Please Enter Your DateOfBirth.');
+    showMessage('Please Select Your DateOfBirth');
   } else {
     let dateInt = parseInt(date.replaceAll('-', ''));
     let number = luckyNumber.value;
     if (number === '') {
-      alert('Please Enter Your Lucky Number.');
+      showMessage('Please Enter Your Lucky Number');
     } else {
       isTheSumOfDigitsDivisible(dateInt, number);
     }
@@ -46,3 +47,11 @@ policy.addEventListener('click', function () {
     policyCard.className = policyCard.className.replace('show', '');
   }, 3000);
 });
+
+function showMessage(msg) {
+  message.innerText = msg;
+  message.style.display = 'block';
+}
+function hideMessage() {
+  message.style.display = 'none';
+}
